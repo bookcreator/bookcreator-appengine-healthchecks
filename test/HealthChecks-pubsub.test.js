0107c8804b5e2184e89791b5aadf9a7de497c7df
Rich Hodgkins <rich@bookcreator.com>
2017-09-19 14:49:11 +0100

    Fix and tests

--- test/HealthChecks-pubsub.test.js
+++ test/HealthChecks-pubsub.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert')
 const { EventEmitter } = require('events')
 const MockDate = require('mockdate')
+const httpMocks = require('node-mocks-http')
 
 const HealthChecks = require('..')
 
@@ -61,6 +62,51 @@ describe('HealthChecks - pubsub', function() {
 			sub.emit('error', err)
 			
 			assert.strictEqual(hc._healthy.error, err)
+		})
+	})
+	
+	describe('middleware', function() {
+
+		const next = () => next.called = true
+		beforeEach(function() {
+			next.called = false
+		})
+	
+		describe('legacy health checks', function() {
+			describe('default endpoint', function() {
+				it('has no error', function() {
+					const hc = new HealthChecks()
+					hc.startMonitorPubSubSubscription(sub)
+					const req = httpMocks.createRequest({
+						method: 'GET',
+						url: '/_ah/health'
+					})
+					const res = httpMocks.createResponse()
+					
+					hc(req, res, next)
+					
+					assert(res._isEndCalled())
+					assert.strictEqual(res.statusCode, 200, `Invalid error code, response data: ${res._getData()}`)
+					assert(!next.called)
+				})
+				it('has no message error', function() {
+					const hc = new HealthChecks()
+					MockDate.set(now - (2 * hc.maxSubscriptionQuietPeriodMs))
+					hc.startMonitorPubSubSubscription(sub)
+					MockDate.set(now)
+					const req = httpMocks.createRequest({
+						method: 'GET',
+						url: '/_ah/health'
+					})
+					const res = httpMocks.createResponse()
+					
+					hc(req, res, next)
+					
+					assert(res._isEndCalled())
+					assert.strictEqual(res.statusCode, 500, `Invalid error code, response data: ${res._getData()}`)
+					assert(!next.called)
+				})
+			})
 		})
 	})
 })
