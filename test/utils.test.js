ef68b12a4c6e844e12f1aa7953c0e275d38e204b
Rich Hodgkins <rhodgkins@gmail.com>
2019-10-03 16:21:44 +0100

    Updated google dependencies and removed pre v1 pubsub support

--- test/utils.test.js
+++ test/utils.test.js
@@ -15,42 +15,7 @@ describe('utils.ageOfMessage', function () {
 		MockDate.reset()
 	})
 
-	it('valid messages v0.11 (timestamp)', function () {
-
-		const mockMessage = (s = 0, m = 0, h = 0, d = 0) => {
-			let ms = d * 24 * 60 * 60.0
-			ms += h * 60 * 60
-			ms += m * 60
-			ms += s
-			ms *= 1000
-
-			const message = {
-				timestamp: new Date(now - ms)
-			}
-
-			return utils.ageOfMessage(message)
-		}
-
-		assert.strictEqual(mockMessage(0), '0.000s')
-		assert.strictEqual(mockMessage(0.0001), '0.001s')
-		assert.strictEqual(mockMessage(0.00049), '0.001s')
-		assert.strictEqual(mockMessage(0.0005), '0.001s')
-		assert.strictEqual(mockMessage(0.01), '0.010s')
-		assert.strictEqual(mockMessage(1), '1.000s')
-		assert.strictEqual(mockMessage(59, 4), '299.000s')
-		assert.strictEqual(mockMessage(0, 5), '5m 0.000s')
-		assert.strictEqual(mockMessage(1, 5), '5m 1.000s')
-		assert.strictEqual(mockMessage(59, 59, 1), '119m 59.000s')
-		assert.strictEqual(mockMessage(0, 0, 2), '2h 0m 0.000s')
-		assert.strictEqual(mockMessage(1, 0, 2), '2h 0m 1.000s')
-		assert.strictEqual(mockMessage(59, 59, 23), '23h 59m 59.000s')
-		assert.strictEqual(mockMessage(0, 0, 0, 1), '1d 0h 0m 0.000s')
-		assert.strictEqual(mockMessage(1, 0, 0, 1), '1d 0h 0m 1.000s')
-		assert.strictEqual(mockMessage(56.981, 40, 20, 2), '2d 20h 40m 56.981s')
-		assert.strictEqual(mockMessage(12.345, 50, 10, 12), '12d 10h 50m 12.345s')
-	})
-
-	it('valid messages ^0.22.2 (publishTime)', function () {
+	it('valid messages ^1.1.0 (.publishTime)', function () {
 
 		const mockMessage = (s = 0, m = 0, h = 0, d = 0) => {
 			let ms = d * 24 * 60 * 60.0
@@ -98,14 +63,9 @@ const pubsub_package = {
 	lib: new (require('@google-cloud/pubsub').PubSub)(), // eslint-disable-line global-require
 	get version() { return this.lib.options.libVersion }
 }
-const pubsub_v0_11 = {
-	from: 'google-cloud',
-	lib: require('google-cloud').pubsub(), // eslint-disable-line global-require
-	get version() { return this.lib.userAgent.split('/')[1] }
-}
 
 describe('versions of set pub-subs', function () {
-	for (const { from, lib, version } of [pubsub_package, pubsub_v0_11]) {
+	for (const { from, lib, version } of [pubsub_package]) {
 		it(`${from} should be valid lib`, function () {
 			assert(lib)
 		})
@@ -117,7 +77,7 @@ describe('versions of set pub-subs', function () {
 
 describe('utils.isPubSubSubscription', function () {
 
-	for (const { lib, version } of [pubsub_package, pubsub_v0_11]) {
+	for (const { lib, version } of [pubsub_package]) {
 
 		it(`valid object - ${version}`, function () {
 			const sub = lib.subscription('sub-name')
